79275f757b08cb54c9388937544bd637b89e4671
mariams58 <mariams58@yahoo.com>
2024-10-25 08:38:10 +0000

    Mysql: task 3 fix 1

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists band with Glam Rock as their main style
 -- Ranks bands by lifespan
 
-SELECT band_name, IFNULL(split, 2022) - formed AS nb_fans
+SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam Rock%'
 ORDER BY lifespan DESC;
